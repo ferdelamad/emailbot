c947189f178e9741e2cbbff51d9ad19dab6d011f
Fernando De La Madrid <ferdelamad@gmail.com>
2019-02-23 15:44:08 -0800

    Add async and state validation middlewares

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+
+import async from '../middleware/async';
+import stateValidator from '../middleware/stateValidator';
 import rootReducer from '../reducers/rootReducer';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
-  return createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+  return createStore(rootReducer, composeEnhancer(applyMiddleware(async, stateValidator)));
 }
